763fbbec0f4932cccd663f62feaaad00e8a6dfed
Jerome Layat <fujeyla@gmail.com>
2018-09-24 21:12:56 +0200

    license header

--- Processing4Netbeans/src/org/netbeans/modules/java/processingproject/ProcessingContribsNodeFactory.java
+++ Processing4Netbeans/src/org/netbeans/modules/java/processingproject/ProcessingContribsNodeFactory.java
@@ -1,3 +1,21 @@
+/**
+ *   This file is part of Processing4Netbeans Netbeans plugin, 
+ *   initially created by fujeyla.
+ *
+ *   Processing4Netbeans is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Processing4Netbeans is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with Processing4Netbeans.  If not, see <https://www.gnu.org/licenses/>
+ */
+
 package org.netbeans.modules.java.processingproject;
 
 import java.util.ArrayList;
@@ -8,7 +26,6 @@ import org.netbeans.modules.java.j2seproject.J2SEProject;
 import org.netbeans.spi.project.ui.support.NodeFactory;
 import org.netbeans.spi.project.ui.support.NodeList;
 import org.openide.filesystems.FileObject;
-import org.openide.loaders.DataNode;
 import org.openide.loaders.DataObject;
 import org.openide.loaders.DataObjectNotFoundException;
 import org.openide.nodes.FilterNode;
